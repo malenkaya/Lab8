ffc9c61ede4e1f7a4fa4e88ee645793b31e462e5
Mukhanbetaly Samat <samat.muhanbetaly@gmail.com>
2022-11-08 19:39:34 +0500

    first samat change for new Tree

--- lab8.py
+++ lab8.py
@@ -1,10 +1,22 @@
 #hello guys
 
 # Самат начинаю функцию
-
+"""
 def summa(a , b):
     return a+b
 
 a = int(input("Введите первое число : "))
 b = int(input("Введите второе число : "))
 print("Сумма двух чисел = ",summa(a,b))
+"""
+# Добавляем новуя функция и перемещяем его в новую ветку
+def intervalArray(a,b):
+    A = set()
+    for i in range(a,b+1):
+        A.add(i)
+    return sorted(A)
+
+
+a = int(input("Введите начальную точку : "))
+b = int(input("Введите окончательную точку : "))
+print(intervalArray(a,b))
