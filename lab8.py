9b2eb1cd91f4731dfc8f4fa60fc0b7cf7f0150ec
Mukhanbetaly Samat <samat.muhanbetaly@gmail.com>
2022-11-08 19:06:55 +0500

    second samat change

--- lab8.py
+++ lab8.py
@@ -1,1 +1,10 @@
 #hello guys
+
+# Самат начинаю функцию
+
+def summa(a , b):
+    return a+b
+
+a = int(input("Введите первое число : "))
+b = int(input("Введите второе число : "))
+print("Сумма двух чисел = ",summa(a,b))
