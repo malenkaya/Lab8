e9f99ac0f7530fb174b2e3808ce8576bf1b83ff1
Dilnaz <74405096+malenkaya@users.noreply.github.com>
2022-11-08 20:31:39 +0600

    labcopy

--- lab8copy.py
+++ lab8copy.py
@@ -1,1 +1,12 @@
-#dgd
+#hello guys
+
+# Самат начинаю функцию
+
+def summa(a , b):
+    return a+b
+
+a = int(input("Введите первое число : "))
+b = int(input("Введите второе число : "))
+print("Сумма двух чисел = ",summa(a,b))
+
+#3 commit
